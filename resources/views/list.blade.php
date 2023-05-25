295e43a11e2715556619f3e8201da1234d330ef2
eriktailor <erno22szabo@gmail.com>
2023-05-25 06:12:04 +0200

    Show id update

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,6 +1,7 @@
 @foreach ($students as $student)
     <li class="list-group-item">
         <div class="p-3">
+            <span>{{ $student->id.'. ' }}</span>
             <b>{{ $student->name }}</b> 
             <a class="link-opacity-75-hover" href="#">{{ $student->email }}</a>
         </div>
